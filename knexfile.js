8dffa6196e2ee40dc4ab50a42944212e984da755
Adam <65742386+adamdasovich@users.noreply.github.com>
2021-04-30 10:25:02 -0400

    added pool to file

--- knexfile.js
+++ knexfile.js
@@ -4,9 +4,15 @@ module.exports = {
 
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true,
     connection: {
       filename: './data/lessons.db3'
     },
-    useNullAsDefault: true
-  },
+    polll: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      }
+    }
+    
+  }
 };
