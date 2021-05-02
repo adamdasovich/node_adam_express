82ad5c5698313f3e54f06c0ff353f40b64feb99d
Adam <65742386+adamdasovich@users.noreply.github.com>
2021-05-02 08:39:19 -0400

    changed location

--- knexfile.js
+++ knexfile.js
@@ -17,8 +17,10 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: 'postgres://dfhhtommiamqjm:69f56c67f8f36fd164936712cc544991aca60c7607c024629460f26b3dabfd32@ec2-54-235-108-217.compute-1.amazonaws.com:5432/d2gjc58ugrdbek'
-    pool: {
+    connection: {
+      filename: 'postgres://dfhhtommiamqjm:69f56c67f8f36fd164936712cc544991aca60c7607c024629460f26b3dabfd32@ec2-54-235-108-217.compute-1.amazonaws.com:5432/d2gjc58ugrdbek'
+    },
+      pool: {
       min: 2,
       max: 10
     },
